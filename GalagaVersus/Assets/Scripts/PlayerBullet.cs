6cd22450aedde82c24ae84a1afc0deea757e65bb
alicechi2 <alicechi2@gmail.com>
2020-02-25 13:31:24 -0800

    style(PlayerBulletCode): Add more comments and change variable names to make code more readable

--- GalagaVersus/Assets/Scripts/PlayerBullet.cs
+++ GalagaVersus/Assets/Scripts/PlayerBullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
+    // Establish local variable speed within the class PlayerBullet
     float speed;
     // Start is called before the first frame update
     void Start()
@@ -14,19 +15,20 @@ public class PlayerBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Get bullet curr pos
+        // Get the bullet's current position
         Vector2 position = transform.position;
 
-        //Calculate new pos
+        // Calculate the x, y coordinates for the new position
         position = new Vector2(position.x, position.y + speed * Time.deltaTime);
 
-        //Update bullet pos
+        // Update the position of the bullet using the position variable above
         transform.position = position;
 
-        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+        // Calculate the maximum position that the bullet can travel to (end of screen)
+        Vector2 maxPos = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
 
-        //If Bullet outside sreen, destroy
-        if(transform.position.y > max.y)
+        // If Bullet travels outside of the frame, destroy the gameobject
+        if(transform.position.y > maxPos.y)
         {
             Destroy(gameObject);
         }
