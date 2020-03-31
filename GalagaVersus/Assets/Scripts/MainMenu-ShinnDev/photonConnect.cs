9d0d41ee3718db0ad0d9d23bf8e3e39c496ce77f
staniya <staniya@thacher.org>
2020-03-30 21:42:53 -1000

    feat(photonConnect): adding panels and script to allow players to connect to Photon server

---
+++ GalagaVersus/Assets/Scripts/MainMenu-ShinnDev/photonConnect.cs
@@ -0,0 +1,58 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class photonConnect : MonoBehaviour
+{
+    // Restrict players with different versions of the game to
+    // join the same server
+    public string versionName = "0.1";
+
+    public GameObject connectPanel;
+    public GameObject joinPanel;
+    public GameObject disconnectPanel;
+
+    // The function that allows players to access the server
+    // Passes in the versionName as a parameter
+    public void connectToPhoton(){
+        PhotonNetwork.ConnectUsingSettings(versionName);
+
+        Debug.Log("Connecting to Photon server...");
+    }
+
+    // When players join the master server or the default game lobby,
+    // they should be able to see the username of the other players in the lobby
+    public void OnConnectedToMaster(){
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+
+        Debug.Log("We are connected to master");
+    }
+
+    // A function that lets users join a lobby
+    private void OnJoinedLobby(){
+        // When the player joins the lobby, the panel with the button
+        // to join the lobby should no longer be active, and the lobby
+        // panel should be activated
+        connectPanel.SetActive(false);
+        joinPanel.SetActive(true);
+
+        Debug.Log("On Joined Lobby");
+    }
+
+    // When the player gets disconnected from the server, run this funciton
+    private void OnDisconnectedFromPhoton(){
+
+        // Only set active the panel that displays the error message
+        if(connectPanel.SetActive(true))
+            connectPanel.SetActive(false);
+
+        if(joinPanel.SetActive(true))
+            joinPanel.SetActive(false);
+
+        disconnectPanel.SetActive(true);
+
+        Debug.Log("Disconnected from photon services");
+    }
+}
