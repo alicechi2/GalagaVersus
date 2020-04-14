3ee32ba5ddaee9691efdaff21f7bdc659918f41b
staniya <staniya@thacher.org>
2020-04-13 19:31:01 -1000

    feat(NetworkConnectionManager): implementing new gameObject buttons that react when server is disconnected

--- GalagaVersus/Assets/Scripts/Shinn MainMenu Dev/NetworkConnectionManager.cs
+++ GalagaVersus/Assets/Scripts/Shinn MainMenu Dev/NetworkConnectionManager.cs
@@ -11,7 +11,6 @@ public class NetworkConnectionManager : MonoBehaviourPunCallbacks
     // Declare the MainMenu Gameobjects as public variables
     public Button BtnJoinRoom;
     public Button BtnConnectMaster;
-    public Canvas CanvasDisconnectFromMaster;
     public Button BtnJoinWhenDisconnect;
     public Button BtnQuitWhenDisconnect;
     public Text TextDisconnectNotifier;
@@ -21,7 +20,6 @@ public class NetworkConnectionManager : MonoBehaviourPunCallbacks
     public bool ConnectToMasterAttempt;
     public bool ConnectToRoomAttempt;
     public bool Disconnected;
-    public bool CanvasDisplayed;
 
     // Data Values for Room settings
     [SerializeField]
@@ -29,14 +27,16 @@ public class NetworkConnectionManager : MonoBehaviourPunCallbacks
     [SerializeField]
     private bool roomVisible = true;
 
+    // Variable keep track of roomName declared by players
+    public string roomName;
+
     // Use this for initialization
     void Start()
     {
-        // Initially set these four to false
+        // Initially set these two to false
         ConnectToMasterAttempt = false;
         ConnectToRoomAttempt = false;
         Disconnected = false;
-        CanvasDisplayed = false;
     }
 
     // Update is called once per frame
@@ -46,27 +46,39 @@ public class NetworkConnectionManager : MonoBehaviourPunCallbacks
         BtnConnectMaster.gameObject.SetActive(!PhotonNetwork.IsConnected && !ConnectToRoomAttempt);
         BtnJoinRoom.gameObject.SetActive(PhotonNetwork.IsConnected && !ConnectToMasterAttempt && !ConnectToRoomAttempt);
 
-        // The Canvas has to be set active when all the conditions are true, and the game is not connected to the Photon server
-        CanvasDisconnectFromMaster.gameObject.SetActive(!PhotonNetwork.IsConnected && !Disconnected);
         // The buttons and text on the canvas are set active when the game is disconnected and the canvas is displayed
-        BtnJoinWhenDisconnect.gameObject.SetActive(!Disconnected && !CanvasDisplayed);
-        BtnQuitWhenDisconnect.gameObject.SetActive(!Disconnected && !CanvasDisplayed);
-        TextDisconnectNotifier.gameObject.SetActive(!Disconnected && !CanvasDisplayed);
+        BtnJoinWhenDisconnect.gameObject.SetActive(!PhotonNetwork.IsConnected && Disconnected);
+        BtnQuitWhenDisconnect.gameObject.SetActive(!PhotonNetwork.IsConnected && Disconnected);
+        TextDisconnectNotifier.gameObject.SetActive(!PhotonNetwork.IsConnected && Disconnected);
     }
 
     // When the button ConnectToMaster is pressed
     public void OnClickConnectToMaster() 
     {
+        ConnectToMasterAttempt = true;
+
         // Settings for configuring the Photon Network
-        PhotonNetwork.OfflineMode = false; // Setting this to truw ould fake an online connection
+        PhotonNetwork.OfflineMode = false; // Setting this to true would fake an online connection
         PhotonNetwork.NickName = "PlayerName"; // This is the settings to configure a player nickname
         PhotonNetwork.AutomaticallySyncScene  = true; //to call PhotonNetwork.LoadLevel()
         PhotonNetwork.GameVersion = "v1"; // This is to restrict players with different versions from playing together
 
         ConnectToMasterAttempt = true;
+
         // In case the automatic connection fails to work this is the function call for the manual connection
         // PhotonNetwork.ConnectToMaster(ip, port, 62e2b6f7-09e9-495c-92fa-24bf602b565d)
-        PhotonNetwork.ConnectUsingSettings(); //automatic connection based on the config file in Photon/PhotonUnityNetworking/Resourcs/PhotonServerSettings.asset
+        if (!PhotonNetwork.OfflineMode)
+            PhotonNetwork.ConnectUsingSettings(); //automatic connection based on the config file in Photon/PhotonUnityNetworking/Resourcs/PhotonServerSettings.asset
+    }
+
+    // When the user successfully connects to the master server
+    public override void OnConnectedToMaster() 
+    {
+        base.OnConnectedToMaster();
+        // There are no more attempts necessary, so set this condition to false
+        ConnectToMasterAttempt = false;
+        // Print out that connection was successful
+        Debug.Log("Connection successful to Master server");
     }
 
     // When the something goes wrong with connecting to the server
@@ -84,16 +96,6 @@ public class NetworkConnectionManager : MonoBehaviourPunCallbacks
         Debug.Log(cause);
     }
 
-    // When the user successfully connects to the master server
-    public override void OnConnectedToMaster() 
-    {
-        base.OnConnectedToMaster();
-        // There are no more attempts necessary, so set this condition to false
-        ConnectToMasterAttempt = false;
-        // Print out that connection was successful
-         Debug.Log("Connection successful to Master server");
-    }
-
     // When the user clicks the button to connect to a room
     public void OnClickConnectToRoom()
     {
@@ -104,6 +106,9 @@ public class NetworkConnectionManager : MonoBehaviourPunCallbacks
 
         ConnectToRoomAttempt = true;
 
+        // PhotonNetwork.CreateRoom("{0}", roomName); // Create a specific Room - Error: OnCreateRoomFailed
+        // PhotonNetwork.JoinRoom("{0}", roomName); // Join a specific Room - Error: OnJoinRoomFailed
+
         // Have Photon guide the suer to a random room
         PhotonNetwork.JoinRandomRoom(); // - Error: OnJoinedRandomRoomFailed
     }
@@ -112,20 +117,20 @@ public class NetworkConnectionManager : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-        ConnectToRoomAttempt = true;
+        ConnectToRoomAttempt = false;
 
         // One player is always set as master so that he or she can make executive decisions
         // On the console, print out the number of players who are currently in the game lobby
-        Debug.Log("Master: " + PhotonNetwork.IsMasterClient + " | PLayers in Room: " + PhotonNetwork.CurrentRoom.PlayerCount);
+        Debug.Log("Master: " + PhotonNetwork.IsMasterClient + " | Players in Room: " + PhotonNetwork.CurrentRoom.PlayerCount);
         // After the Room is Joined, it will launch the scene GameEngine to play the game
-        SceneManager.LoadScene("GameEngine"); 
+        if(PhotonNetwork.IsMasterClient && SceneManager.GetActiveScene().name != "Main")
+            PhotonNetwork.LoadLevel("GameEngine"); 
     }
 
     // When JoinRandomRoom() fails which means that there are no available rooms for the player
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         base.OnJoinRandomFailed(returnCode, message);
-        ConnectToRoomAttempt = true;
 
         // This could signify that no rooms are currently available
         // Create a new join room where the name of the room is temporarily set to null
@@ -146,14 +151,14 @@ public class NetworkConnectionManager : MonoBehaviourPunCallbacks
         ConnectToRoomAttempt = false;
     }
 
-    // When the user gets disconnected and decides to Quit the game
-    public override void OnLeftLobby()
-    {
-        base.OnLeftLobby();
-        // If the player decides to quit, set disconnect to false again
-        Disconnected = false;
+    // // When the user gets disconnected and decides to Quit the game
+    // public override void OnLeftLobby()
+    // {
+    //     base.OnLeftLobby();
+    //     // If the player decides to quit, set disconnect to false again
+    //     Disconnected = false;
 
-        // Print out that the user decided to quit
-         Debug.Log("Player chose to quit game when server disconnected");
-    }
+    //     // Print out that the user decided to quit
+    //      Debug.Log("Player chose to quit game when server disconnected");
+    // }
 }
