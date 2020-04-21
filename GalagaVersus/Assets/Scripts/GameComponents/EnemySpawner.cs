698dace6bd95fe125854e5c3eedcf0847d77e547
staniya <staniya@thacher.org>
2020-04-20 23:47:20 -1000

    Revert "feat(enemy AI): added beginning enemy choreographed movements"
    This reverts commit 66eddb126a19b6b6dcb0669a6a9006269e7be7c8.

--- GalagaVersus/Assets/Scripts/GameComponents/EnemySpawner.cs
+++ GalagaVersus/Assets/Scripts/GameComponents/EnemySpawner.cs
@@ -1,23 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using PathCreation;
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject EnemySprite; // Our enemy prefab
-    public PathCreator[] EnemyPaths = new PathCreator[20]; // Makes an array of 20 paths
-    public float maxSpawnRateInSeconds = 2f; // How quickly the enemies spawn
+    float maxSpawnRateInSeconds = 5f;
     // Start is called before the first frame update
-    int index = 0;
-
-    private IEnumerator coroutine; // Defines a coroutine
-
-    // Makes the WaitAndSpawn function the coroutine and then starts it
     void Start()
     {
-        coroutine = WaitAndSpawn(maxSpawnRateInSeconds);
-        StartCoroutine(coroutine);
+        Invoke("SpawnEnemy", maxSpawnRateInSeconds);
+
+        // increase spawn rate every 30 s
+        InvokeRepeating("IncreaseSpawnRate", 0f, 30f);
     }
 
     // Update is called once per frame
@@ -26,17 +21,41 @@ public class EnemySpawner : MonoBehaviour
         
     }
 
-
-    // Creates and instantiates the enemiy game objects and sets each of their paths, waiting in between each creation
-    private IEnumerator WaitAndSpawn(float waitTime)
+    void SpawnEnemy()
     {
-        while (index < EnemyPaths.Length)
+        // Sets where to spawn the enemy
+        Vector2 min = Camera.main.ViewportToWorldPoint( new Vector2((float) 0.25,0));
+
+        Vector2 max = Camera.main.ViewportToWorldPoint( new Vector2((float) 0.75,1));
+
+        // Spawns the enemy
+        GameObject anEnemy = (GameObject)Instantiate(EnemySprite);
+        anEnemy.transform.position = new Vector2(Random.Range (min.x, max.x), max.y);
+
+        // Schedule when to spawn enemy
+        ScheduleNextEnemySpawn();
+    }
+
+    void ScheduleNextEnemySpawn()
+    {
+        // Determines when to spawn the enemy
+        float spawnInSeconds;
+        if (maxSpawnRateInSeconds > 1f)
         {
-            yield return new WaitForSeconds(waitTime);
-            GameObject anEnemy = (GameObject)Instantiate(EnemySprite);
-            anEnemy.transform.position = new Vector2(-10,-10);
-            anEnemy.GetComponent<EnemyControl>().enemySpawnPath = EnemyPaths[index];
-            index++;
+            spawnInSeconds = Random.Range(1f, maxSpawnRateInSeconds);
+        }
+        else {
+            spawnInSeconds = 1f;
+        } 
+        Invoke("SpawnEnemy", spawnInSeconds);
+    }
+
+    // For a hard mode i think
+    void IncreaseSpawnRate() {
+        if (maxSpawnRateInSeconds > 1f){
+            maxSpawnRateInSeconds--;
+        } if (maxSpawnRateInSeconds == 1f){
+            CancelInvoke("IncreaseSpawnRate");
         }
     }
 }
